f72fc035698389b1f5f99e8b52d35957f17dcd83
kjohn01 <r01631002@ntu.edu.tw>
2017-12-16 01:22:52 +0800

    style cover section

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,26 @@
+var $nav = $(".navbar");
+var $cover = $("header");
+var $top = $("#backToTop");
+var $nextSec = $("#nextSec");
 // animation
 new WOW().init();
 // nav cantrol
 $(function () {
   $(document).scroll(function () {
-    var $nav = $(".navbar");
-    var $cover = $("header");
-    var $top = $("#backToTop");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $cover.height());
-    $top.toggleClass('scrolled', $(this).scrollTop() > $cover.height());
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $cover.height()-$nav.height());
+    $top.toggleClass('scrolled', $(this).scrollTop() > $cover.height()-$nav.height());
+    $nextSec.toggleClass('scrolled', $(this).scrollTop() > 0);
   });
 });
 // backToTop
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
     document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+// nextSec
+function nextSecFunction() {
+  document.body.scrollTop = $cover.height()-$nav.height(); // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = $cover.height()-$nav.height(); // For IE and Firefox
 }
 // browser detection
 $(function () {
