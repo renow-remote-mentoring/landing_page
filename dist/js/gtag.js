fb1b28236958f1a41e14f03b6ca78fd6758a25bf
kjohn01 <r01631002@ntu.edu.tw>
2017-12-19 19:26:51 +0800

    fb insight and social sharing meta

--- dist/js/gtag.js
+++ dist/js/gtag.js
@@ -1,1 +1,5 @@
-function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-110965898-1");
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-110965898-1');
--- src/js/fbInsight.js
+++ src/js/fbInsight.js
@@ -2,15 +2,17 @@ window.fbAsyncInit = function() {
     FB.init({
       appId      : '299019057284878',
       xfbml      : true,
-      version    : 'v2.10'
+      version    : 'v2.11'
     });
+
     FB.AppEvents.logPageView();
+
   };
 
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
