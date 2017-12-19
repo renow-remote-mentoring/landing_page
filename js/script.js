17cce291a42ab2ef142be6463ba6b83c4675a179
kjohn01 <r01631002@ntu.edu.tw>
2017-12-19 19:26:51 +0800

    fb insight and social sharing meta

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}function nextSecFunction(){document.body.scrollTop=$cover.height()-$nav.height(),document.documentElement.scrollTop=$cover.height()-$nav.height()}var $nav=$(".navbar"),$cover=$("header"),$top=$("#backToTop"),$nextSec=$("#nextSec");(new WOW).init(),$(function(){$(document).scroll(function(){$nav.toggleClass("scrolled",$(this).scrollTop()>$cover.height()-$nav.height()),$top.toggleClass("scrolled",$(this).scrollTop()>$cover.height()-$nav.height()),$nextSec.toggleClass("scrolled",$(this).scrollTop()>0)})}),$(function(){-1!=navigator.userAgent.indexOf("Safari")&&-1==navigator.userAgent.indexOf("Chrome")&&($(".navbar").addClass("safari"),$("#mc_embed_signup_scroll").addClass("safari"))});
+var $nav = $(".navbar");
+var $cover = $("header");
+var $top = $("#backToTop");
+var $nextSec = $("#nextSec");
+// animation
+new WOW().init();
+// nav cantrol
+$(function () {
+  $(document).scroll(function () {
+    $nav.toggleClass('scrolled', $(this).scrollTop() > ($cover.height()-$nav.height()));
+    $top.toggleClass('scrolled', $(this).scrollTop() > ($cover.height()-$nav.height()));
+    $nextSec.toggleClass('scrolled', $(this).scrollTop() > 0);
+  });
+});
+// backToTop
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+// nextSec
+function nextSecFunction() {
+  document.body.scrollTop = ($cover.height()-$nav.height()); // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = ($cover.height()-$nav.height()); // For IE and Firefox
+}
+// browser detection
+$(function () {
+if (navigator.userAgent.indexOf('Safari') != -1 &&
+    navigator.userAgent.indexOf('Chrome') == -1) {
+        $(".navbar").addClass("safari");
+        $("#mc_embed_signup_scroll").addClass("safari");
+    }
+});
