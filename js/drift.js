d9e3960c99f32bd7b013ebada56f6ec21bb962d1
kjohn01 <r01631002@ntu.edu.tw>
2018-01-26 00:01:47 +0800

    change mentor card CTA text and alignment, fix drift, update navbar.

--- js/drift.js
+++ js/drift.js
@@ -18,3 +18,8 @@
 }();
 drift.SNIPPET_VERSION = '0.3.1';
 drift.load('e32rmse4z4yy');
+drift.on('ready',function(api, payload) {
+  // if (window.innerWidth <= 780) {
+  //  api.widget.hide()
+  // }
+})
